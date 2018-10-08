0a78915c68e896798530e87561673181183b541a
lcbfree <349429034@qq.com>
2018-10-08 23:05:15 +0800

    添加pojo

---
+++ e3-manager/e3-manager-pojo/src/main/java/com/e3shop/pojo/GitTest.java
@@ -0,0 +1,13 @@
+package com.e3shop.pojo;
+
+public class GitTest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
